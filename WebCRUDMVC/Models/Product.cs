900c7e614ed43dc47d7c1e10e4a2eacb3758bbab
David <davidbarbosa425@gmail.com>
2022-01-28 19:25:06 -0300

    first commit

---
+++ WebCRUDMVC/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCRUDMVC.Models
+{
+    [Table("product")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "Code")]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
